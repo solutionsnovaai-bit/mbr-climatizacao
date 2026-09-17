7af7efdac39d56ce40fe7b5017a303a260f2f3c2
solutionsnovaai-bit <solutions.nova.ai@gmail.com>
2026-09-16 21:13:00 -0300

    Delete src directory

--- src/lib/whatsapp.ts
+++
@@ -1,5 +0,0 @@
-import { SITE } from '../config/site'
-
-export function waLink(message: string = SITE.defaultMessage) {
-  return `https://wa.me/${SITE.phoneDigits}?text=${encodeURIComponent(message)}`
-}
